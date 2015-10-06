ee4600f27f0ecea9b0a8306a91b61e5827ef6e8a
Ruslan Bilych <r.bilych@gmail.com>
2015-10-06 09:59:12 +0300

    add validations for Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,13 @@
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate :text_fields_not_same
+
+  protected
+
+  def text_fields_not_same
+    if original_text.downcase == translated_text.downcase
+      errors.add(:original_text,
+                 'Оригинальный текст не должен быть равен переведенному')
+    end
+  end
 end
