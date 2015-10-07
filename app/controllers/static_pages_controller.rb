a68cfa7d90e1fc7c1457e6e8e95c0037514f8fd2
Ruslan Bilych <r.bilych@gmail.com>
2015-10-07 16:25:02 +0300

    add simple trainer feature

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def index
+    @card = Card.rand_card.first
+  end
+
+  def answer
+    alert = Card.check_answer params[:answer],
+                              params[:original_text],
+                              params[:current_card_id]
+
+    redirect_to root_path, alert: alert
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,24 @@ class Card < ActiveRecord::Base
   validate :text_fields_not_same
 
   before_validation on: :create do
-    self.review_date ||= 3.days.from_now
+    self.review_date ||= change_review_date
+  end
+
+  scope :rand_card, -> { where("review_date <= :today", today: Date.today)
+                        .order("RANDOM()")
+                        .limit(1) }
+
+  def self.check_answer(answer, original_text, current_card_id)
+    if answer.downcase.strip == original_text.downcase.strip
+      Card.find(current_card_id).update(review_date: change_review_date)
+      alert = "Правильно"
+    else
+      alert = "Неправильно"
+    end
+  end
+
+  def self.change_review_date
+    3.days.from_now
   end
 
   protected
@@ -11,7 +28,7 @@ class Card < ActiveRecord::Base
   def text_fields_not_same
     if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
       errors.add(:original_text,
-                 'Оригинальный текст не должен быть равен переведенному')
+                 "Оригинальный текст не должен быть равен переведенному")
     end
   end
 end
