b80470334992786e72a277418afe7a4618a3601f
Ruslan Bilych <r.bilych@gmail.com>
2015-10-07 09:29:31 +0300

    refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,14 @@
 require "open-uri"
 
 URL = "http://iloveenglish.ru/vocabulary/verbs-words"
-words = []
 
 page = Nokogiri::HTML(open(URL)).css("div.singers tr>td:nth-child(2)")
 page.search("span").remove
 
 page.each do |word|
-  words << word.content if word.content != ""
-end
-
-words.each do |word|
-  w = word.split(/—/)
-  Card.create(original_text: w[0].strip, translated_text: w[1].strip)
+  w = word.content if word.content != ""
+  unless w.nil?
+    w.split(/—/)
+    Card.create(original_text: w[0].strip, translated_text: w[1].strip)
+  end
 end
