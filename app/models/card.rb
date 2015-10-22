573465f056b0cc2ffd0a1229e55c3a3fc3353c99
Ruslan Bilych <r.bilych@gmail.com>
2015-10-22 19:54:47 +0300

    refactor check_answer

--- app/models/card.rb
+++ app/models/card.rb
@@ -20,16 +20,16 @@ class Card < ActiveRecord::Base
 
   def check_answer(answer)
     if (result = prepare_string(answer) == prepare_string(original_text))
-      change_box
+      b = box < 5 ? box + 1 : box
     else
       typos = typos_count(answer, original_text)
       return { result: false, typos: true } if typos <= 2
 
       update(mistakes: mistakes + 1)
-      reset_box if mistakes == 3
+      b = mistakes == 3 ? 1 : false
     end
 
-    change_review_date
+    change_review_date(b)
 
     { result: result, typos: false }
   end
@@ -41,16 +41,11 @@ class Card < ActiveRecord::Base
                                 prepare_string(original_text), 0)
   end
 
-  def change_box
-    update(box: box + 1, mistakes: 0) if box < 5
-  end
-
-  def reset_box
-    update(box: 1, mistakes: 0)
-  end
-
-  def change_review_date
-    update(review_date: Time.now.getlocal + TIME[box - 1])
+  def change_review_date(b)
+    if b
+      time = Time.now.getlocal + TIME[b - 1]
+      update(box: b, mistakes: 0, review_date: time)
+    end
   end
 
   def text_fields_not_same
