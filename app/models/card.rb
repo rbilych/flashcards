665cba1306febeabde02ed1d859c62bc079fd986
Ruslan Bilych <r.bilych@gmail.com>
2015-10-23 15:25:22 +0300

    fix

--- app/models/card.rb
+++ app/models/card.rb
@@ -36,7 +36,7 @@ class Card < ActiveRecord::Base
 
   def self.send_notify
     User.all.each do |user|
-      if user.cards.for_review.count && user.email > 0
+      if user.cards.for_review.count > 0 && user.email
         NotificationsMailer.pending_cards(user).deliver_now
       end
     end
