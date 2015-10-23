33a80b079360aab644c37d44ed61460b7d9c0b07
Ruslan Bilych <r.bilych@gmail.com>
2015-10-23 14:48:24 +0300

    fix environments config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,12 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "flashcardsss.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
 
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "flashcardsss.herokuapp.com" }
 end
