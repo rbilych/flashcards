cca43e68b53ce316e55fea14a0171b7eab31b04a
Ruslan Bilych <r.bilych@gmail.com>
2015-10-27 12:41:59 +0200

    fix mailer

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -3,7 +3,10 @@ class NotificationsMailer < ApplicationMailer
     @cards_count = user.cards.for_review.count
     @cards = t("email.card", count: @cards_count)
 
-    mail(to: user.email,
-         subject: default_i18n_subject(count: @cards_count, cards: @cards))
+    I18n.with_locale(user.locale) do
+      mail(to: user.email,
+           subject: t("notifications_mailer.pending_cards.subject",
+                      count: @cards_count, cards: @cards))
+    end
   end
 end
