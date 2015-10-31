cea41fa2a839e030f4d20202a5862c7382f43459
Ruslan Bilych <r.bilych@gmail.com>
2015-10-31 13:03:33 +0200

    change public ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '172.31.26.76', user: 'deploy', roles: %w{web app db}
+server '50.112.175.144', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
