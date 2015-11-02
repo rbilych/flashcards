62cd76f0adf4a99d9fb194afcfa67ac4b74d8334
Ruslan Bilych <r.bilych@gmail.com>
2015-11-02 16:28:16 +0200

    amazon aim

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server '52.32.203.89', user: 'deploy', roles: %w{app db web}, primary: true
+server '52.26.70.14', user: 'deploy', roles: %w{app db web}, primary: true
 
 set :rails_env, :production
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
@@ -18,9 +18,9 @@ set :rails_env, :production
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{deploy@52.32.203.89}
-role :web, %w{deploy@52.32.203.89}
-role :db,  %w{deploy@52.32.203.89}
+role :app, %w{deploy@52.26.70.14}
+role :web, %w{deploy@52.26.70.14}
+role :db,  %w{deploy@52.26.70.14}
 
 
 
