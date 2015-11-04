eb5318a4d129558712e752f173a517209d84321d
Ruslan Bilych <r.bilych@gmail.com>
2015-11-04 12:29:58 +0200

    redesign dashboard section

--- app/controllers/dashboard/cards_controller.rb
+++ app/controllers/dashboard/cards_controller.rb
@@ -1,11 +1,9 @@
 class Dashboard::CardsController < ApplicationController
   before_action :find_card, only: [:show, :edit, :update, :destroy]
+  before_action :find_deck, only: [:update, :destroy]
 
   def index
     @cards = current_user.cards
-  end
-
-  def show
   end
 
   def new
@@ -18,13 +16,14 @@ class Dashboard::CardsController < ApplicationController
   def create
     if deck_params[:title].blank?
       @card = current_user.cards.build(card_params)
+      @deck = current_user.decks.find(@card.deck_id)
     else
       @deck = current_user.decks.create(deck_params)
       @card = @deck.cards.build(card_params)
     end
 
     if @card.save
-      redirect_to @card
+      redirect_to @deck
     else
       render :new
     end
@@ -32,7 +31,7 @@ class Dashboard::CardsController < ApplicationController
 
   def update
     if @card.update(card_params)
-      redirect_to @card
+      redirect_to @deck
     else
       render :edit
     end
@@ -40,13 +39,17 @@ class Dashboard::CardsController < ApplicationController
 
   def destroy
     @card.destroy
-    redirect_to cards_path
+    redirect_to @deck
   end
 
   private
 
   def find_card
     @card = current_user.cards.find(params[:id])
+  end
+
+  def find_deck
+    @deck = current_user.decks.find(@card.deck_id)
   end
 
   def card_params
